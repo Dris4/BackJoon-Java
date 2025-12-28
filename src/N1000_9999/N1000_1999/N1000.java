31b6db1eaca76ac083e77e9e8270f1bcae472741
Dris <97157931+Dris4@users.noreply.github.com>
2025-12-28 15:25:22 +0900

    -

---
+++ src/N1000_9999/N1000_1999/N1000.java
@@ -0,0 +1,7 @@
+package N1000_9999.N1000_1999;
+
+public class N1000 {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/N1000_9999/N2000_2999/N2000.java
@@ -0,0 +1,7 @@
+package N1000_9999.N2000_2999;
+
+public class N2000 {
+    public static void main(String[] args){
+
+    }
+}
