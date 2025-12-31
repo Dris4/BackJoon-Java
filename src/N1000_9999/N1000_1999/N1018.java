278dc0fe74f104c4ae07c5c19b673d65aa941dcd
Dris <97157931+Dris4@users.noreply.github.com>
2025-12-31 23:21:29 +0900

    -

--- src/N1000_9999/N1000_1999/N1018.java
+++ src/N1000_9999/N1000_1999/N1018.java
@@ -4,70 +4,42 @@ import java.util.*;
 
 public class N1018{
     public static void main(String[] args){
-        //정상적인 보드
-        String[][] boardW = {
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"}
-        };
-        String[][] boardB = {
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"}
-        };
-
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
         sc.nextLine();
 
         //체스판 배열로 받기
-        String[][] arr = new String[N][M];
+        char[][] arr = new char[N][M];
         for (int i = 0; i < N; i++) {
             String line = sc.nextLine();
             for(int j = 0; j < M; j++) {
-                arr[i][j] = line.charAt(j) + "";
+                arr[i][j] = line.charAt(j);
             }
         }
 
-
-
         //M - ? = 8, ? = M - 8
-        ArrayList<Integer> result = new ArrayList<>();
+        int min = 64;
         for (int i = 0; i < N-7; i++) {
             for(int j = 0; j < M-7; j++) {
 
                 //부분 체스판과 정상 체스판 비교
-                int maxW = 0;
-                int maxB = 0;
+                int max = 0;
+                char start = arr[i][j];
                 for(int y=0; y<8; y++){
                     for(int x=0; x<8; x++){
-                        if(!arr[i+y][j+x].equals(boardW[y][x])){
-                            maxW++;
-                        }
-
-                        if(!arr[i+y][j+x].equals(boardB[y][x])) {
-                            maxB++;
+                        //[0, 0]이 W라 가정
+                        if(((y+x)%2==0 && arr[i+y][j+x]!='W')
+                                || ((y+x)%2!=0 && arr[i+y][j+x]!='B')){
+                            max++;
                         }
                     }
                 }
-                result.add(maxW);
-                result.add(maxB);
-                maxW = 0;
-                maxB = 0;
-
+                max = Math.min(max, 64-max);
+                min = Math.min(min, max);
+                max = 0;
             }
         }
-        System.out.println(Collections.min(result));
+        System.out.println(min);
     }
 }
