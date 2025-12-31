fc9ea1c6a4a1c2219e473da5e76eb3047b636340
Dris <97157931+Dris4@users.noreply.github.com>
2025-12-31 20:31:44 +0900

    -

--- src/N1000_9999/N1000_1999/N1000.java
+++ src/N1000_9999/N1000_1999/N1000.java
@@ -1,7 +1,12 @@
 package N1000_9999.N1000_1999;
 
+import java.util.Scanner;
+
 public class N1000 {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
     }
 }
--- src/N1000_9999/N2000_2999/N2000.java
+++
@@ -1,7 +0,0 @@
-package N1000_9999.N2000_2999;
-
-public class N2000 {
-    public static void main(String[] args){
-
-    }
-}
